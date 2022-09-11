afcca2088eeb5ff79e3b9f56a1382cd5397cd1da
the-intern <ostclasses@gmail.com>
2022-09-11 09:17:08 -0500

    add readme to strings module

--- no_pull_commit.sh
+++ no_pull_commit.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-git add -A
-git commit -m "$*"
-git push
+printf "\n\nYou must use git tags for this.\n\n"
+printf "Something like:\n\tgit tag -a part-i-strings -m 'part-i strings'"
+echo ""
+# git add -A
+# git commit -m "$*"
+# git push
 
 
