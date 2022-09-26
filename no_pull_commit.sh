f0514f12085047daed820c1e2e36723f34b07425
the-intern <ostclasses@gmail.com>
2022-09-26 13:39:43 -0500

    tweak the part iii practice procs notebook

--- no_pull_commit.sh
+++ no_pull_commit.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-printf "\n\nYou must use git tags for this.\n\n"
-printf "Something like:\n\tgit tag -a part-i-strings -m 'part-i strings'"
-echo ""
-# git add -A
-# git commit -m "$*"
-# git push
+git add -A
+git commit -m "$*"
+git push
 
 
